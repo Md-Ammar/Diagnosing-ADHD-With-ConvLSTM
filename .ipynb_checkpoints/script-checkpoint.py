79822224c035a4434f8334e5656793766b232097
ammar <ammaratif559@gmail.com>
2024-04-11 21:25:47 +0530

    new repo

---
+++ .ipynb_checkpoints/script-checkpoint.py
@@ -0,0 +1,20 @@
+import os
+import re
+import shutil
+
+data_dir = r"D:\Peking_1_preproc\Peking_1"
+
+os.chdir(data_dir)
+files = os.listdir()
+
+dest_dir = r"D:\Peking_1"
+
+for file in files:
+    cur_dir = data_dir+f"\\{file}" 
+    os.chdir(cur_dir)
+
+    for f in os.listdir(cur_dir):
+        m = re.match("(snwmrda|sfnwmrda).+(nii.gz)", str(f))
+        if m:
+            print(m.group())
+            shutil.copy(cur_dir+"\\"+str(m.group()), dest_dir)
